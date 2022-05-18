bf12349da95c0116d45ff0b47440e030f2a2e4de
Hang1809 <hangdinhphankhanh@gmail.com>
2022-05-19 04:58:10 +0700

    Official CaseStudyM2

---
+++ src/vn/hangdinh/bohoshop/Main.java
@@ -0,0 +1,10 @@
+package vn.hangdinh.bohoshop;
+
+import vn.hangdinh.bohoshop.views.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+       UserManagerView.signIn();
+    }
+}
